6beaecd28a0e595225a40ce0f97698339de819fa
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-05-03 11:40:29 -0300

    Header and dashboard summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header/Index";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle/>
-      <h1>Hello world</h1>
-    </div>
+      <Header/>
+      <Dashboard/>
+    </>
   );
 }
 
