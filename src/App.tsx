c63667afa663e84028c547aa7bf07e254b45a8f5
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-05-03 21:10:54 -0300

    Transactions table and fake api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header/Index";
+import {createServer} from 'miragejs'
 import { GlobalStyle } from "./styles/global";
-
+createServer({
+  routes(){
+    this.namespace = 'api'
+    this.get('/transactions',()=>{
+      return[
+        {
+          id:1,
+          title:"transaction",
+          amount: 400,
+          type: 'deposit',
+          category:'foot',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 export function App() {
   return (
     <>
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,12 @@
 import { Summary } from "../Summary"
+import { TransactionsTable } from "../TransactionsTable"
 import { Container } from "./styles"
 
 export const Dashboard = () => {
   return (
     <Container>
       <Summary/>
-
+      <TransactionsTable/>
     </Container>
   )
 }
