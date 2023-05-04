b4eb48fdb3a28c5327831a23e3ca8a7b47e3cfd7
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-05-04 10:53:47 -0300

    Styles finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header/Index";
 import {createServer} from 'miragejs'
+import Modal from 'react-modal'
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 createServer({
   routes(){
     this.namespace = 'api'
@@ -20,11 +23,20 @@ createServer({
   }
 })
 export function App() {
+  Modal.setAppElement('#root')
+  const [transactionModalOpen,setTransactionModalOpen] = useState(false)
+  function handleOpenNewTransactionModal(){
+  setTransactionModalOpen(true)
+  }
+  function handleCloseNewTransactionModal(){
+  setTransactionModalOpen(false)
+  }
   return (
     <>
-      <GlobalStyle/>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+      <NewTransactionModal isOpen={transactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
+      <GlobalStyle/>
     </>
   );
 }
