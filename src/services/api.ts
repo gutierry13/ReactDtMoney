ee815267fb81f9a810da204c351d96d56a781677
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-06-01 10:54:54 -0300

    test

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 export const api = axios.create({
-seURL: 'https://react-dt-money-5pt8.vercel.app/api',
+  baseURL: 'https://react-dt-money-5pt8.vercel.app/api',
   
 })
